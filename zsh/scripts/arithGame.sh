42b716b956f0494591aab169c22d592724843306
Ephraim Sutherland <ephraim.sutherland@gmail.com>
2023-11-26 11:55:14 -0800

    added arithGame

--- zsh/scripts/arithGame.sh
+++ zsh/scripts/arithGame.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 
+# array[0]="addi"
+# array[1]="sub"
+# array[2]="multi"
+# array[3]="div"
+
 array=(
 "addi"
 "sub"
@@ -7,7 +12,10 @@ array=(
 "div"
 )
 
+# size=${#array[@]}
 size=${#array[@]}
+#index=$(($RANDOM % $size))
+#echo ${array[$index]}
 
 highScore=0
 
@@ -39,20 +47,21 @@ div () {
 	echo "$multi % $i"; sol=$j; }
 
 MentalMath () {
-	read -p "range: (defaults to 100) " VARIABLE
+	# read -p "range: (defaults to 100) " VARIABLE
 	RANGE=${VARIABLE:=100}
-	read -p "time in seconds: (defaults to two minute) " VARIABLE
-	time=${VARIABLE:=120}
+	# read -p "time in seconds: (defaults to two minute) " VARIABLE
+	# time=${VARIABLE:=120}
+	time=120
 	echo "you have $time seconds"
 	SECONDS=0
 	CurrentCount=0
 	#echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 	while [[ $SECONDS -le $time ]]; do
 		# if [ $i -ge $j ]; then first=$i second=$j; else first=$j second=$i; fi 	
-		index=$(($RANDOM % $size))
+		index=$(($RANDOM % $size+1))
 		${array[$index]}
-		#echo $sol
-		read -p "Answer : " ans
+
+		echo "Answer "; read ans
 		#Note normally numerical equality is -eq but not within 
 		#expansion operator ((...))
 		test=`bc -l <<< "$sol-$ans"` #; if (( $(bc -l <<< "${test#-} <= .1") )) $(( $ans != $sol ))
@@ -60,7 +69,8 @@ MentalMath () {
 		while [ $(bc -l <<< "${test#-} <= .1" ) -eq 0 ]  && [[ $SECONDS -le $time ]]
 		do
 			echo no
-			read -p "Answer : " ans
+			echo "Answer : "
+			read ans
 			test=`bc -l <<< "$sol-$ans"`
 		done
 		CurrentCount=$((CurrentCount+1))
