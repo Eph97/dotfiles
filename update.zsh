525601140629a8d48ba63b8517cc17b83f8191cc
Eph97 <ephraim.sutherland@yale.edu>
2023-12-31 13:18:40 -0500

    commiting snippets

--- update.zsh
+++ update.zsh
@@ -7,6 +7,9 @@ cp ~/.config/bash/.bashrc ~/Documents/dotfiles/bash/.bashrc
 # for i in $HOME/.config/bash/execute/*; do cp $i ./bash/execut; done
 for i in ~/.config/bash/execut/*.sh; do cp $i ./bash/execut/; done
 
+skels=`ls -a $HOME/.config/nvim/snippets/.skeleton.*`
+for i in $skels; do cp $i ./nvim/snippets/; done
+cp ~/.config/nvim/snippets/.math.preamble.tex ./nvim/snippets/.math.preamble.tex
 
 cp ~/.config/nvim/UltiSnips/tex.snippets ~/Documents/dotfiles/nvim/UltiSnips/tex.snippets
 cp ~/.config/nvim/init.vim ~/Documents/dotfiles/nvim/init.vim
@@ -27,3 +30,8 @@ cp ~/.config/i3/i3blocks.conf ~/Documents/dotfiles/i3/i3blocks.conf
 
 cp ~/.scripts/dmonitor ~/Documents/dotfiles/scripts/dmonitor
 cp ~/.scripts/connect_bluetooth.sh ~/Documents/dotfiles/scripts/connect_bluetooth.sh
+
+
+# loop through anonymous files in ~/.config/nvim/snippets/ and copy them to ./nvim/snippets
+#
+
